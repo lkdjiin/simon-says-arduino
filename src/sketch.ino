8d1bd9e17ac70a60a7786d879e503209f664f7b7
lkdjiin <xavier.nayrac@gmail.com>
2015-03-02 18:03:04 +0100

    Code for step 2

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,50 +1,67 @@
 const byte RED = 0;
 const byte GREEN = 1;
+const byte BLUE = 2;
+const byte YELLOW = 3;
 
-byte buttonRed = 8;
-byte buttonGreen = 9;
-byte ledRed = 2;
-byte ledGreen = 3;
+const byte buttonRed = 8;
+const byte buttonGreen = 9;
+const byte buttonBlue = 10;
+const byte buttonYellow = 11;
+const byte ledRed = 2;
+const byte ledGreen = 3;
+const byte ledBlue = 4;
+const byte ledYellow = 5;
+
 byte computerTurn = 1;
 byte lastPly;
 
 void setup() {
   pinMode(buttonRed, INPUT);
   pinMode(buttonGreen, INPUT);
+  pinMode(buttonBlue, INPUT);
+  pinMode(buttonYellow, INPUT);
   pinMode(ledRed, OUTPUT);
   pinMode(ledGreen, OUTPUT);
+  pinMode(ledBlue, OUTPUT);
+  pinMode(ledYellow, OUTPUT);
 
   randomSeed(analogRead(0));
 }
 
 void loop() {
   if(computerTurn) {
-    lastPly = random(2);
+    lastPly = random(4);
     allLedsOff();
     if(lastPly == RED) {
       digitalWrite(ledRed, HIGH);
-    } else {
+    } else if(lastPly == GREEN) {
       digitalWrite(ledGreen, HIGH);
+    } else if(lastPly == BLUE) {
+      digitalWrite(ledBlue, HIGH);
+    } else {
+      digitalWrite(ledYellow, HIGH);
     }
     delay(300);
     allLedsOff();
     computerTurn = 0;
   } else {
-    int stateRed = digitalRead(buttonRed);
-    int stateGreen = digitalRead(buttonGreen);
-    if(stateRed || stateGreen) {
+    byte stateRed = digitalRead(buttonRed);
+    byte stateGreen = digitalRead(buttonGreen);
+    byte stateBlue = digitalRead(buttonBlue);
+    byte stateYellow = digitalRead(buttonYellow);
+    if(stateRed || stateGreen || stateBlue || stateYellow) {
       if(stateRed) {
         lightTheLed(ledRed);
-      } else {
+      } else if(stateGreen) {
         lightTheLed(ledGreen);
+      } else if(stateBlue) {
+        lightTheLed(ledBlue);
+      } else {
+        lightTheLed(ledYellow);
       }
-      if(stateRed && lastPly == RED) {
-        // OK
-        delay(2000);
-      } else if(stateGreen && lastPly == GREEN) {
-        // OK
-        delay(2000);
-      } else if((stateRed && lastPly == GREEN) || (stateGreen && lastPly == RED)) {
+      if(isRightAnswer(stateRed, stateGreen, stateBlue, stateYellow)) {
+        delay(1500);
+      } else {
         blinkLeds();
         delay(2000);
       }
@@ -54,9 +71,21 @@ void loop() {
   }
 }
 
+boolean isRightAnswer(byte red, byte green, byte blue, byte yellow) {
+  if( (red && lastPly == RED) || (green && lastPly == GREEN) ||
+      (blue && lastPly == BLUE) || (yellow && lastPly == YELLOW) ) {
+    return true;
+  } else {
+    return false;
+  }
+
+}
+
 void allLedsOff() {
   digitalWrite(ledRed, LOW);
   digitalWrite(ledGreen, LOW);
+  digitalWrite(ledBlue, LOW);
+  digitalWrite(ledYellow, LOW);
 }
 
 void lightTheLed(byte led) {
@@ -66,11 +95,14 @@ void lightTheLed(byte led) {
 }
 
 void blinkLeds() {
-  for(int i = 0; i < 10; i++) {
+  for(byte i = 0; i < 10; i++) {
     digitalWrite(ledRed, HIGH);
     digitalWrite(ledGreen, HIGH);
+    digitalWrite(ledBlue, HIGH);
+    digitalWrite(ledYellow, HIGH);
     delay(100);
     allLedsOff();
     delay(100);
   }
 }
+
